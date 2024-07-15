f34c94f2dda167cd71cfe22c5961b85f0dd2a792
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-15 16:10:31 +0900

    フッター作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='py-2 px-4 border-t-2'>
+      <small>@2024 next13</small>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './Header '
+import Footer from './Footer'
 
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className='container mx-auto bg-slate-500 text-slate-100'>
         <Header></Header>
         {children}
+        <Footer />
         </body>
     </html>
   )
