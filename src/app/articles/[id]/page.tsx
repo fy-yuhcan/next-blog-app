d1d8a08387259227c2e25952d5db68ddf7d10446
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-15 19:23:55 +0900

    詳細ページの取得API

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { getDetailArticles } from '@/blogAPI'
+import Image from 'next/image'
+import React from 'react'
+
+
+const Article = async({ params }: {params:{id:string}}) => {
+    const detailArticle = await getDetailArticles(params.id)
+  return (
+    <div className='max-w-3xl mx-auto p-5'>
+    <Image src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='' width={1280} height={100} />
+    <h1 className='text-4xl text-center mb-10 mt-10'>{detailArticle.title}</h1>
+    <div className='text-lg leading-relaxed text-justify'>
+        <p>{detailArticle.content}</p>
+    </div>
+    </div>
+  )
+}
+
+export default Article
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]>=>{
     const res = await fetch(`http://localhost:3001/posts`,{cache: "no-store"}) //SSR
@@ -9,4 +10,17 @@ export const getAllArticles = async (): Promise<Article[]>=>{
     await new Promise((resolve)=>setTimeout(resolve,1500))
     const articles = await res.json()
     return articles;
+}
+export const getDetailArticles = async (id: string): Promise<Article>=>{
+    const res = await fetch(`http://localhost:3001/posts/${id}`,{next: {revalidate: 60}}) //ISR
+
+    if(res.status === 404){
+        notFound()
+    }
+
+    if(!res.ok){
+    throw new Error("エラーが出ています")}
+    await new Promise((resolve)=>setTimeout(resolve,1000))
+    const article = await res.json()
+    return article;
 }
