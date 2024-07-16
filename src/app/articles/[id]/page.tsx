e6ddf3176bd917cc6c3a24801f4d8055e8eced7f
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-16 11:09:02 +0900

    詳細ページのAPIを叩いて取得

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -5,7 +5,10 @@ import React from 'react'
 
 
 const Article = async({ params }: {params:{id:string}}) => {
-    const detailArticle = await getDetailArticles(params.id)
+    //const detailArticle = await getDetailArticles(params.id)
+    const API_URL=process.env.NEXT_PUBLIC_API_URL
+  const res = await fetch(`${API_URL}/api/${params.id}`,{next: {revalidate: 10}})
+  const detailArticle =await res.json()
   return (
     <div className='max-w-3xl mx-auto p-5'>
     <Image src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='' width={1280} height={100} />
